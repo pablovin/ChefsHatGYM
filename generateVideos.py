5b5f0d4c03e19f7e475ca93c65c14d27ed82a40d
Pablo <pablo.alvesdebarros@iit.it>
2020-02-29 18:43:16 +0100

    Addding a video example and updating the Readme.md

--- generateVideos.py
+++ generateVideos.py
@@ -1,8 +1,7 @@
 from KEF import RenderManager
 
-dataSetLocation = "/home/pablo/Documents/Datasets/ChefsHat_ReinforcementLearning/Gym_Experiments/Player_4_Cards_11_games_2TrainAgents_['RANDOM', 'RANDOM', 'RANDOM', 'RANDOM']_Joker_Trial_QL_2020-02-29_16:30:17.444821/Datasets/Game_1.csv"
-
+dataSetLocation = "Examples/Example_Random_Agent.csv"
 fps = 25
 
-render = RenderManager.RenderManager("/home/pablo/Documents/Datasets/ChefsHat_ReinforcementLearning/testRender/", fps=fps)
+render = RenderManager.RenderManager("Examples/", fps=fps)
 render.renderGameStatus(dataSetLocation, createVideo=True)

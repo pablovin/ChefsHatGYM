32d4342cf6c8cecdfb154baccaf09b7471ccedb2
Pablo <pablo.alvesdebarros@iit.it>
2020-03-12 19:41:58 +0100

    updated requirements list

--- runExperiment.py
+++ runExperiment.py
@@ -6,13 +6,13 @@ from Agents.AgentType import  DUMMY_RANDOM, DUMMY_DISCARDONECARD, DQL_v1, DQL_v2
 playersAgents = [DQL_v2, DUMMY_RANDOM, DUMMY_RANDOM, DUMMY_RANDOM]
 
 numGames = 1000# amount of training games
-experimentDescriptor = "ReTraining_100x"
+experimentDescriptor = "Training_100x"
 
 # loadModel = "/home/pablo/Documents/Datasets/ChefsHat_ReinforcementLearning/Gym_Experiments/WINNINGDQL_Player_4_Cards_11_games_100TrainAgents_['DQL', 'RANDOM', 'RANDOM', 'RANDOM']_QLLearningAgent_2020-02-29_21:49:50.134777/Model/actor_iteration_27.hd5"
 
-loadModel = "/home/pablo/Documents/Datasets/ChefsHat_ReinforcementLearning/BaselineExperiments/DQL_V2/Player_4_Cards_11_games_50TrainAgents_['DQL_v2', 'DUMMY_RANDOM', 'DUMMY_RANDOM', 'DUMMY_RANDOM']_DQLV2_TargetNetwork_2020-03-03_11:32:23.968124/Model/actor_iteration_49.hd5"
+# loadModel = "/home/pablo/Documents/Datasets/ChefsHat_ReinforcementLearning/BaselineExperiments/DQL_V2/Player_4_Cards_11_games_50TrainAgents_['DQL_v2', 'DUMMY_RANDOM', 'DUMMY_RANDOM', 'DUMMY_RANDOM']_DQLV2_TargetNetwork_2020-03-03_11:32:23.968124/Model/actor_iteration_49.hd5"
 
-# loadModel = ""
+loadModel = ""
 
 # #Parameters for controling the experiment
 isLogging = False #Logg the experiment
